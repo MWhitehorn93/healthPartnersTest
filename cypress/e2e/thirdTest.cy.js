4772cdbd610e090e54c65310241f5602d8024ee3
MWhitehorn93 <matthew.whitehorn@sword-grc.com>
2023-09-20 17:09:26 +0100

    Touch up

--- cypress/e2e/thirdTest.cy.js
+++ cypress/e2e/thirdTest.cy.js
@@ -1,5 +1,6 @@
 describe('Amazon text search test', () => {
     it('Amazon text search test', () => {
+        //The CSS selectors in this test are very poor and this is not what I would use in a commercial setting. In a commercial setting I would enter data-testID all over the site
         //The todays deals square is not located on cypress (I assume it needs a cookie or a user to be logged in). I have used the the macbook resolution for the link to appear on the banner which is just below the search bar
         cy.viewport('macbook-15')
         //Cy visit amazons website
@@ -12,7 +13,7 @@ describe('Amazon text search test', () => {
         //As this is a simple test, the first item is selected. 
         cy.get('[data-testid="grid-deals-container"] > :nth-child(1)').click()
         //As this is a simple test, the first item is selected. 
-        cy.get(':nth-child(1) > .a-list-item > .octopus-dlp-asin-section > .a-spacing-base > .a-link-normal > .a-section').click()
+        cy.get('#octopus-dlp-asin-stream > .a-unordered-list > :nth-child(1) > .a-list-item').click()
         //The add to cart button is clicked
         cy.get('#add-to-cart-button').click()
         //#sc-buy-box-ptc-button > .a-button-inner > .a-button-input is the locator for the proceed to checkout button
